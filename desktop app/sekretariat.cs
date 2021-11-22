0283e2e8d23cec9e6f784e12f753e178c9b68c1c
Visual Student <visualstudent@zschie.edu.pl>
2021-11-22 16:00:33 +0100

    structure done

---
+++ desktop app/sekretariat.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+
+
+namespace desktop_app
+{
+    class szkola
+    {
+
+        List<student> students { get; set; }
+
+        List<teacher> teachers { get; set; }
+
+        List<employee> emplayeess { get; set; }
+
+        class person
+        {
+            string first_name { get; set; }
+            string last_name { get; set; }
+            string mothers_name { get; set; }
+            string fathers_name { get; set; }
+            string gender { get; set; }
+            string pesel { get; set; }
+            DateTime birth_date { get; set; }
+        }
+
+        class group {
+            string name { get; set; }
+        }
+
+        class student : person
+        {
+            group school_class { get; set;}
+        }
+
+        class employee : person
+        {
+            Bitmap photo { get; set; }
+            string job_position { get; set; }
+        }
+
+        class teacher : employee
+        {
+            string supervising { get; set; }
+            List<string> subjects { get; set; }
+            List<KeyValuePair<group, int>> godziny {get; set;}
+            DateTime emplayment_date { get; set; }
+        }
+    }
+}
