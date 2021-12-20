eb52a234612a6703e38b3f95e09e95073c2c7258
SNIAPA <hubertwasilewski69@gmail.com>
2021-12-20 09:45:35 +0100

    added deleting

--- desktop app/MainWindow.xaml.cs
+++ desktop app/MainWindow.xaml.cs
@@ -17,153 +17,6 @@ using System.Diagnostics;
 
 namespace desktop_app
 {
-    public class RelayCommand : ICommand
-    {
-        private Action<object> execute;
-
-        private Predicate<object> canExecute;
-
-        private event EventHandler CanExecuteChangedInternal;
-
-        public RelayCommand(Action<object> execute)
-            : this(execute, DefaultCanExecute)
-        {}
-
-        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
-        {
-            if (execute == null)
-            {
-                throw new ArgumentNullException("execute");
-            }
-            if (canExecute == null)
-            {
-                throw new ArgumentNullException("canExecute");
-            }
-
-            this.execute = execute;
-            this.canExecute = canExecute;
-        }
-
-
-        public event EventHandler CanExecuteChanged
-        {
-            add
-            {
-                CommandManager.RequerySuggested += value;
-                this.CanExecuteChangedInternal += value;
-            }
-            remove
-            {
-                CommandManager.RequerySuggested -= value;
-                this.CanExecuteChangedInternal -= value;
-            }
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return this.canExecute != null && this.canExecute(parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            this.execute(parameter);
-        }
-
-        public void OnCanExecuteChanged()
-        {
-            EventHandler handler = this.CanExecuteChangedInternal;
-            if (handler != null)
-            {
-                handler.Invoke(this, EventArgs.Empty);
-            }
-        }
-
-        public void Destrou()
-        {
-            this.canExecute = _ => false;
-            this.execute = _ => { return; };
-        }
-
-        private static bool DefaultCanExecute(object parameter)
-        {
-            return true;
-        }
-    }
-
-    class MainWindowViewModel
-    {
-        private ICommand importCommand;
-
-        private ICommand toggleExecuteCommand { get; set; }
-
-        private bool canExecute = true;
-
-        public string ImportCommandContent
-        {
-            get
-            {
-                return "asd";
-            }
-        }
-
-        public bool CanExecute
-        {
-            get
-            {
-                return this.canExecute;
-            }
-
-            set
-            {
-                if (this.canExecute == value)
-                {
-                    return;
-                }
-
-                this.canExecute = value;
-            }
-        }
-
-        public ICommand ToggleExecuteCommand
-        {
-            get
-            {
-                return toggleExecuteCommand;
-            }
-            set
-            {
-                toggleExecuteCommand = value;
-            }
-        }
-
-        public ICommand ImportCommand
-        {
-            get
-            {
-                return importCommand;
-            }
-            set
-            {
-                importCommand = value;
-            }
-        }
-
-        public MainWindowViewModel()
-        {
-            ImportCommand = new RelayCommand(ShowMessage, param => this.canExecute);
-            toggleExecuteCommand = new RelayCommand(ChangeCanExecute);
-        }
-
-        public void ShowMessage(object obj)
-        {
-            MessageBox.Show(obj.ToString());
-        }
-
-        public void ChangeCanExecute(object obj)
-        {
-            canExecute = !canExecute;
-        }
-    }
 
     /// <summary>
     /// Interaction log ic for MainWindow.xaml
@@ -172,8 +25,11 @@ namespace desktop_app
     {
 
         School school;
+        public class Tasks : ObservableCollection<Task>
+        {
+            // Creating the Tasks collection in this way enables data binding from XAML.
+        }
 
-        
         public MainWindow()
         {
             InitializeComponent();
@@ -182,13 +38,15 @@ namespace desktop_app
 
             student_list.ItemsSource = school.students;
 
+            importButton.Click += ImportButton_Click;
 
+            student_list.
 
         }
 
-        private void Buttton_Clicked(object sender,RoutedEventArgs e)
+        private void ImportButton_Click(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine("asd");
+            Debug.WriteLine(school.students.Count);
         }
 
 
